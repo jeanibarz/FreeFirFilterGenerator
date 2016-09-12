a1a11d58b89c7c1b2b941c010e7e9df8f3d048e0
Jean Ibarz <ibarz.jean@gmail.com>
2016-09-12 07:21:57 +0200

    Update of .gitignore + some minor modifications
    Desactivation of group delay auto suggest checkbox, added setting to
    save the output directory when you save an impulse.

---
+++ src/freefirfiltergenerator/FreeFirFilterGenerator.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package freefirfiltergenerator;
+
+/**
+ *
+ * @author Jean
+ */
+public class FreeFirFilterGenerator {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /* Check if debugging mode */
+        if(args.length >= 1 && args[0].equals("-debug")) {
+            FreeFirFilterGeneratorForm.DEBUG_MODE = true;
+            System.out.println("Debugging mode...");
+        }
+        
+        FreeFirFilterGeneratorForm mainForm = new FreeFirFilterGeneratorForm();
+
+        if(FreeFirFilterGeneratorForm.DEBUG_MODE) {
+            double[] w = freefirfiltergenerator.Window.triangular(10);
+
+            for(int i = 0; i < w.length; i++) System.out.println("W[" + i + "]=" + w[i]);
+        }
+        
+        mainForm.setVisible(true);
+    }
+    
+}
---
+++ src/freefirfiltergenerator/filter/FilterTypeEnum.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package freefirfiltergenerator.filter;
+
+/**
+ *
+ * @author Jean
+ */
+public enum FilterTypeEnum {
+    LINKWITZ_RILEY,
+    BUTTERWORTH,
+    UNDEFINED
+}
